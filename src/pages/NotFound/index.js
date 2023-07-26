4d31c42e429c641133c6fbb403d37112c97c92e8
ribeiro-jr <ribeirojr1522@gmail.com>
2023-07-26 10:43:08 -0300

    Página de not found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,10 +1,26 @@
-
+import './notfound.css';
+import Logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
 
   return (
-    <section>
-      Página de Error
+    <section className='not'>
+      <section className='container-not'>
+        <header className='header-login not-found'>
+          <h1>4</h1>
+          <img src={Logo} alt='logo' />
+          <h1>4</h1>
+        </header>
+        <section className='conent-not'>
+
+          <p>Ops!! Página não encontrada</p>
+          <Link to="/dashboard">
+            Página Inicial
+          </Link>
+        </section>
+
+      </section>
     </section>
   )
 }
